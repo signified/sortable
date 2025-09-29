5a3f9840b2671314601cd31a4e2c7faf32a7aa4a
Robert Love <robertjohnlove@gmail.com>
2025-09-29 17:05:43 +1000

    Update sortable.js

--- sortable.js
+++ sortable.js
@@ -2,13 +2,9 @@ function sortable(selectors, options) {
 
   let defaultOptions = {
     buttonClasses: ['sortable-button'],
-    sortClasses: ['sortable-sort'],
-    sortStringClasses: ['sortable-sort-string'],
-    sortNumberClasses: ['sortable-sort-number'],
-    sortDateClasses: ['sortable-sort-date'],
-    sortDotDecimalClasses: ['sortable-sort-dot-decimal'],
-    sortAscendingClasses: ['sortable-sort-ascending'],
-    sortDescendingClasses: ['sortable-sort-descending']
+    sortedClasses: ['sortable-sorted'],
+    sortedAscendingClasses: ['sortable-sorted-ascending'],
+    sortedDescendingClasses: ['sortable-sorted-descending']
   };
 
   options = {
@@ -17,44 +13,42 @@ function sortable(selectors, options) {
   };
 
   const getDataType = function(tBody, cellIndex) {
-
-    let dataType = 'string';
+    let dataType = 'default';
     let values = [];
-
     for (let row of tBody.rows) {
-      for (i = 0; i < row.cells.length; i++) {
+      for (let i = 0; i < row.cells.length; i++) {
         if (i == cellIndex) {
           values.push(row.cells[i].textContent);
         }
       }
     }
-
     if (values.every(isDate)) {
       dataType = 'date';
     }
-
-    if (values.every(isNumber)) {
-      dataType = 'number';
-    }
-
-    for (let value of values) {
-      const parts = value.split('.');
-      if (parts.length >= 3 && parts.every(isNumber)) {
-        dataType = 'dot-decimal';
-      }
-    }
-
     return dataType;
-  }
+  };
 
   const isDate = function(value) {
-    const date = new Date(value);
-    return (date !== 'Invalid Date') && isNumber(date);
-  }
+    const isValidDate = (date) => date instanceof Date && !isNaN(date.getTime());
+    if (value instanceof Date) {
+      return isValidDate(value);
+    }
+    if (typeof value === "string") {
+      const dateStringRegex = /^[0-9A-Za-z\s:\-\/,]+$/;
+      if (!dateStringRegex.test(value)) return false;
+      const date = new Date(value);
+      return isValidDate(date);
+    }
+    if (typeof value === "number") {
+      const date = new Date(value);
+      return isValidDate(date);
+    }
+    return false;
+  };
 
-  const isNumber = function(value) {
-    return !isNaN(value);
-  }
+  const sortDefault = function(first, second) {
+    return first.localeCompare(second, navigator.languages[0] || navigator.language, {numeric: true, ignorePunctuation: true});
+  };
 
   const tables = document.querySelectorAll(selectors);
 
@@ -86,18 +80,18 @@ function sortable(selectors, options) {
         }
 
         for (let row of table.rows) {
-          for (i = 0; i < row.cells.length; i++) {
+          for (let i = 0; i < row.cells.length; i++) {
             let cell = row.cells[i];
             for (let option in options) {
               cell.classList.remove(...options[option]);
             }
             cell.removeAttribute('aria-sort');
             if (i == cellIndex) {
-              cell.classList.add(...options.sortClasses);
+              cell.classList.add(...options.sortedClasses);
               if (direction == 'ascending') {
-                cell.classList.add(...options.sortAscendingClasses);
+                cell.classList.add(...options.sortedAscendingClasses);
               } else {
-                cell.classList.add(...options.sortDescendingClasses);
+                cell.classList.add(...options.sortedDescendingClasses);
               }
             }
           }
@@ -109,55 +103,18 @@ function sortable(selectors, options) {
 
           const aCell = a.cells[cellIndex];
           const bCell = b.cells[cellIndex];
-          let aCellContent = aCell.textContent.toLowerCase();
-          let bCellContent = bCell.textContent.toLowerCase();
+
+          let aCellContent = aCell.textContent.toLowerCase().trim();
+          let bCellContent = bCell.textContent.toLowerCase().trim();
 
           switch (dataType) {
             case 'date':
-              th.classList.add(...options.sortDateClasses);
-              aCell.classList.add(...options.sortDateClasses);
-              bCell.classList.add(...options.sortDateClasses);
               aCellContent = new Date(aCellContent);
               bCellContent = new Date(bCellContent);
-              if (direction == 'ascending') {
-                return aCellContent - bCellContent;
-              } else {
-                return bCellContent - aCellContent;
-              }
-              break;
-            case 'number':
-              th.classList.add(...options.sortNumberClasses);
-              aCell.classList.add(...options.sortNumberClasses);
-              bCell.classList.add(...options.sortNumberClasses);
-              aCellContent = new Number(aCellContent);
-              bCellContent = new Number(bCellContent);
-              if (direction == 'ascending') {
-                return aCellContent - bCellContent;
-              } else {
-                return bCellContent - aCellContent;
-              }
-              break;
-            case 'dot-decimal':
-              th.classList.add(...options.sortDotDecimalClasses);
-              aCell.classList.add(...options.sortDotDecimalClasses);
-              bCell.classList.add(...options.sortDotDecimalClasses);
-              aCellContent = aCellContent.split('.').map(n => +n+100000).join('.');
-              bCellContent = bCellContent.split('.').map(n => +n+100000).join('.');
-              if (direction == 'ascending') {
-                return aCellContent.localeCompare(bCellContent);
-              } else {
-                return bCellContent.localeCompare(aCellContent);
-              }
+              return (direction == 'ascending') ? aCellContent - bCellContent : bCellContent - aCellContent;
               break;
             default:
-              th.classList.add(...options.sortStringClasses);
-              aCell.classList.add(...options.sortStringClasses);
-              bCell.classList.add(...options.sortStringClasses);
-              if (direction == 'ascending') {
-                return aCellContent.localeCompare(bCellContent);
-              } else {
-                return bCellContent.localeCompare(aCellContent);
-              }
+              return (direction == 'ascending') ? sortDefault(aCellContent, bCellContent) : sortDefault(bCellContent, aCellContent)
           }
 
         });
